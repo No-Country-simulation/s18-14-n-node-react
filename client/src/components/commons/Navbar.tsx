90c665d3b1385494fa1777525661c940bb5d4edf
Milton Pulido <milton.pulido2018@gmail.com>
2024-10-09 19:05:03 -0500

    common component card

--- client/src/components/commons/Navbar.tsx
+++ client/src/components/commons/Navbar.tsx
@@ -23,13 +23,13 @@ export const NavBarWeb = ({
   return (
     <div
       className={
-        "bg-[#c5c5c5] pr-[104px] pl-[104px] flex flex-row gap-6 items-center justify-start h-[100px] relative " +
+        "bg-[#c5c5c5] pr-[104px] pl-[104px] flex flex-row gap-6 items-center justify-start h-[100px] w-screen relative " +
         className
       }
     >
       <div className="shrink-0 w-[291px] h-[95px] static">
         <div className="bg-[#d9d9d9] w-[291px] h-[95px] absolute left-[104px] top-[2.5px]"></div>
-        <SeccionNavBar className="!absolute !left-[158px] !top-[29.5px]" nameSection="LOREM IPSUM"></SeccionNavBar>
+        <SeccionNavBar className="!absolute !left-[158px] !top-[29.5px]" nameSection="LOGO"></SeccionNavBar>
       </div>
       <div className="flex flex-row gap-6 items-center justify-start flex-1 relative">
         <SeccionNavBar className="!shrink-0" nameSection="LOREM IPSUM"></SeccionNavBar>
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,11 +1,17 @@
 import Footer from "@/components/commons/Footer";
 import Navbar from "@/components/commons/Navbar";
+import { RecetTarget } from "@/components/commons/RecetTarget";
 import { Outlet } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-between">
+    <div className="h-screen w-screen flex flex-col items-center justify-between bg-[#EEE]">
       <Navbar />
+      <RecetTarget 
+        imgSrc="https://dgari.com/wp-content/uploads/2022/02/Flan-de-vainilla-con-caramelo.png"
+        recetName="Flan"
+        recetText="Uno de los postres más sencillos y rápidos de preparar es este flan sabor vainilla con caramelo quemado.."
+      />
       <Outlet />
       <Footer />
     </div>
