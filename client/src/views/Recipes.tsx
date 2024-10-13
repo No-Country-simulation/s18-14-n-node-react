498d8afe069a0112b9080bc54df1109d421cd979
Pablo Spector <pablo_spector@hotmail.com>
2024-10-13 12:38:18 -0300

    correcciones

--- client/src/views/Recipes.tsx
+++ client/src/views/Recipes.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/commons/Footer";
 import Navbar from "@/components/commons/Navbar";
 import { Outlet } from 'react-router-dom'
 
-export default function RecipePersonalized() {
+export default function Recipes() {
   return (
     <div className="w-screen flex flex-col items-center justify-start bg-[#EEE]">
       <Navbar />
