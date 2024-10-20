3e0cbf78477ae95c9eb967d96cc6411dea550798
Milton Pulido <milton.pulido2018@gmail.com>
2024-10-20 17:27:43 -0500

    se crea boton de sesion en navbar

--- client/src/components/Botones/BtnSesion.tsx
+++ client/src/components/Botones/BtnSesion.tsx
@@ -1,20 +1,43 @@
-import { MouseEventHandler } from "react"
+import { MouseEventHandler } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-interface IBtnSesionProps{
+interface IBtnSesionProps {
     action: MouseEventHandler;
     imgSrc?: string;
 }
 
-export const BtnSesion = (props:IBtnSesionProps) =>{
-    return(
-        <button 
-          onClick={props.action} 
-          className="flex items-center min-w-max px-2 gap-3 rounded-lg h-1/2 my-auto focus:outline-none relative bg-colorbeig">
-          <img 
-            src={props.imgSrc} 
-            className="w-10 h-10 rounded-md" 
-          />
-          <p className="text-colorencabezados"><i className="fa-solid fa-angle-down"></i></p>
-        </button> 
+export const BtnSesion = (props: IBtnSesionProps) => {
+    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const logout = () => {
+        localStorage.removeItem("tkn");
+        navigate('/')
+    }
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+        <button
+            onClick={toggleDropdown}
+            className="flex items-center min-w-max px-2 gap-3 rounded-lg h-1/2 my-auto focus:outline-none relative bg-colorbeig">
+            <img
+                src={props.imgSrc}
+                className="w-10 h-10 rounded-md"
+            />
+            <p className="text-colorencabezados"><i className="fa-solid fa-angle-down"></i></p>
+        </button>
+
+        {isOpen && (
+            <div className="absolute right-0 mt-20 w-48 bg-white rounded-lg shadow-custom z-10">
+              <a href="/" onClick={logout} className="block px-4 py-2 rounded-lg text-colorencabezados hover:bg-coloracento">Cerrar sesión</a>
+            </div>
+          )
+        }
+        </>
     )
 }
