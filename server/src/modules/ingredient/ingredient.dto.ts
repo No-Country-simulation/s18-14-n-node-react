94c84fe11f27ff27280370f29b700fd40815ed4e
Billy <fx4038@gmail.com>
2024-10-22 15:55:10 -0400

    feat: adding recipe & ingredients features

---
+++ server/src/modules/ingredient/ingredient.dto.ts
@@ -0,0 +1,12 @@
+export class CreateIngredientDto {
+  private constructor(
+    public readonly name: string,
+    public readonly amount: string,
+  ) {}
+  static create(object: { [key: string]: any }): [string?, CreateIngredientDto?] {
+    const { name, amount } = object;
+    if (!name) return ['Missing name'];
+    if (!amount) return ['Missing amount'];
+    return [undefined, new CreateIngredientDto(name, amount)];
+  }
+}
---
+++ server/src/modules/ingredient/ingredient.service.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+import { CreateIngredientDto } from '../ingredient/ingredient.dto';
+import { PaginationDto } from '../shared/pagination.dtos';
+import { CustomError } from '../errors/custom.errors';
+const prisma = new PrismaClient();
+export class IngredientService {
+  constructor() {}
+  async createIngredient(createIngredientDto: CreateIngredientDto) {
+    try {
+      const ingredient = await prisma.ingredient.create({
+        data: {
+          ...createIngredientDto,
+        },
+      });
+      return ingredient;
+    } catch (error) {
+      throw CustomError.InternalServerError(`${error}`);
+    }
+  }
+  async getIngredients(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    try {
+      const total = await prisma.ingredient.count();
+      const ingredients = await prisma.ingredient.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+      return {
+        page: page,
+        limit: limit,
+        total: total,
+        next: `/api/ingredients?page=${page + 1}&limit=${limit}`,
+        prev: page - 1 > 0 ? `/api/ingredients?page=${page - 1}&limit=${limit}` : null,
+        ingredients: ingredients,
+      };
+    } catch (error) {
+      throw CustomError.InternalServerError('Internal Server Error');
+    }
+  }
+  async deleteIngredient(ingredientId: string) {
+    const ingredientExists = await prisma.ingredient.findUnique({ where: { id: ingredientId } });
+    if (!ingredientExists) throw CustomError.badRequest('Ingredient does not exist');
+    try {
+      await prisma.ingredient.delete({ where: { id: ingredientId } });
+      return { message: 'Ingredient deleted successfully' };
+    } catch (error) {
+      throw CustomError.InternalServerError(`${error}`);
+    }
+  }
+}
