a470db7ad9d3b3d218fe1de343802e428f30d411
Teresa <mtmaisterra@gmail.com>
2024-10-25 21:57:10 -0300

    estilando

--- client/src/components/contacto/Contacto.tsx
+++ client/src/components/contacto/Contacto.tsx
@@ -7,63 +7,65 @@ const Contacto = () => {
         console.log('Mensaje enviado');
     }
     return (
-        <div className="w-[1440px] h-[1024px] bg-background flex flex-row">
-            <div className="w-[571px] h-[470px] mt-[202px] ml-[104px] gap-12">
-                <div className="w-[571px] h-[140px] items-start mb-[50px]">
-                    <h1 className="text-5xl text-head_text leading-[56px] mb-3 text-left font-bold">
+        <div className="lg:w-[1440px] lg:h-[1024px] bg-background flex w-[834px] h-[1252px]">
+            <div className="lg:w-[571px] lg:h-[470px] lg:mt-[202px] lg:ml-[104px] lg:gap-[50px] m-[50px] flex lg:flex-col justify-between">
+                <div className="lg:w-[571px] lg:h-[140px] items-start w-[361px] lg:flex-col">
+                    <h1 className="lg:text-5xl text-head_text lg:leading-[56px] text-left font-bold mb-3 text-4xl h:[80px] leading-[40px]">
                         Ponte en contacto con nosotros
                     </h1>
-                    <p className="text-[16px] font-normal w-[579px] h-[72px] text-subtitle_text leading-6 text-left">
+                    <p className="lg:text-[16px] font-normal lg:w-[579px] lg:h-[72px] text-subtitle_text lg:leading-6 w-full h-auto leading-8 mr:auto text-sm text-left">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius tempor
                         incididunt ut labore et dolore magna aliqua. Ut enim adiqua minim veniam
                         quis nostrud exercitation ullamco
                     </p>
-                </div>                
-                    <div className='flex w-[441px] h-[70px] mb-[35px]'>
-                        <div className="w-[70px] h-[70px] items-center bg-accent70 rounded-[5px]">
-                            <img src={house} className='m-[19px] w-8 h-8' />
+                </div>
+                <div className='ml-[60px]'>
+                    <div className='flex lg:w-[441px] lg:h-[70px] mb-[35px]'>
+                        <div className="lg:w-[70px] lg:h-[70px] w-[40px] h-[40px] items-center bg-accent70 rounded-[5px]">
+                            <img src={house} className='lg:m-[19px] lg:w-8 lg:h-8 w-[18px] h-[18px] m-[10px]' />
                         </div>
-                        <div className='flex-col ml-6 items-center w-[347px]'>
-                            <p className='text-lg mb-3 text-body_text text-justify leading-8'>Nuestra ubicación</p>
-                            <p className='tracking-[0.5px] text-[16px] text-primary_text_color w-[347px] leading-6'>123, calle sin nombre, Buenos Aires, Argentina</p>
-                        </div>
-                    </div>
-                    <div className='flex w-[441px] h-[70px] mb-[35px]'>
-                        <div className="w-[70px] h-[70px] items-center bg-accent70 rounded-[5px]">
-                            <img src={phone} className='m-[19px] w-8 h-8' />
-                        </div>
-                        <div className='flex-col ml-6 items-center w-[347px]'>
-                            <p className='text-lg mb-3 text-body_text text-justify leading-8'>Teléfono</p>
-                            <p className='tracking-[0.5px] text-[16px] text-primary_text_color w-[347px] leading-6'>(+54) 11 1231234</p>
+                        <div className='flex-col ml-6 items-center lg:w-[347px] '>
+                            <p className='lg:text-lg text-[16px] lg:mb-3 text-body_text text-justify leading-8'>Nuestra ubicación</p>
+                            <p className='lg:tracking-[0.5px] lg:text-[16px] text-[14px] text-primary_text_color lg:w-[347px] lg:leading-6 w-[284px] leading-8'>123, calle sin nombre, Buenos Aires, Argentina</p>
                         </div>
                     </div>
-                    <div className='flex w-[441px] h-[70px] mb-[35px]'>
-                        <div className="w-[70px] h-[70px] items-center bg-accent70 rounded-[5px]">
-                            <img src={envelope} className='m-[19px] w-8 h-8' />
+                    <div className='flex lg:w-[441px] lg:h-[70px] mb-[35px]'>
+                        <div className="lg:w-[70px] lg:h-[70px] w-[40px] h-[40px] items-center bg-accent70 rounded-[5px]">
+                            <img src={phone} className='lg:m-[19px] lg:w-8 lg:h-8 w-[18px] h-[18px] m-[10px]' />
                         </div>
-                        <div className='flex-col ml-6 items-center w-[347px]'>
-                            <p className='text-lg mb-3 text-body_text text-justify leading-8'>Correo electrónico</p>
-                            <p className='tracking-[0.5px] text-[16px] text-primary_text_color w-[347px] leading-6'>info@recetas.com</p>
-                        </div>                    
+                        <div className='flex-col ml-6 items-center lg:w-[347px]'>
+                            <p className='lg:text-lg text-[16px] lg:mb-3 text-body_text text-justify leading-8'>Teléfono</p>
+                            <p className='lg:tracking-[0.5px] lg:text-[16px] text-[14px] text-primary_text_color lg:w-[347px] lg:leading-6 w-[284px] leading-8'>(+54) 11 1231234</p>
+                        </div>
+                    </div>
+                    <div className='flex lg:w-[441px] lg:h-[70px] mb-[35px]'>
+                        <div className="lg:w-[70px] lg:h-[70px] w-[40px] h-[40px] items-center bg-accent70 rounded-[5px]">
+                            <img src={envelope} className='lg:m-[19px] lg:w-8 lg:h-8 w-[18px] h-[18px] m-[10px]' />
+                        </div>
+                        <div className='flex-col ml-6 items-center lg:w-[347px]'>
+                            <p className='lg:text-lg text-[16px] lg:mb-3 text-body_text text-justify leading-8'>Correo electrónico</p>
+                            <p className='lg:tracking-[0.5px] lg:text-[16px] text-[14px] text-primary_text_color lg:w-[347px] lg:leading-6 w-[284px] leading-8'>info@recetas.com</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='mt-[202px] ml-[141px] mr-[154px] rounded-[8px] w-[470px] h-[530px] bg-card_background flex-col shadow-md'>
-                <div className='w-[370] h-[430] mt-8 mx-12 my-0'>
-                    <input type='text' className="w-[370px] h-[50px] border border-primary50 rounded-lg  pl-5 py-3 mb-8 text-lg items-center" placeholder='Nombre' />
-                    <input type='email' className="w-[370px] h-[50px] border border-primary50 rounded-lg   pl-5 py-3 mb-8 text-lg items-center" placeholder='Correo electrónico' />
-                    <input type='tel' className="w-[370px] h-[50px] border border-primary50 rounded-lg   pl-5 py-3 mb-8 text-lg items-center" placeholder='Teléfono' />
-                    <input className="w-[370px] h-[130px] border border-primary50 rounded-lg  text-start pl-5 py-3 mb-8 text-lg flex-start" placeholder='Escribe tu mensaje' />
+            <div>
+                <div className='lg:mt-[202px] w-[470px] h-[530px] bg-card_background shadow-lg rounded-lg'>
+                    <input type='text' className="w-[370px] h-[50px] border border-primary50 rounded-lg mt-[50px] mx-[50px] px-4 py-3 mb-[25px] text-[16px] items-center" placeholder='Nombre' />
+                    <input type='email' className="w-[370px] h-[50px] border border-primary50 rounded-lg mx-[50px] px-4 py-3 mb-[25px] text-[16px] items-center" placeholder='Correo electrónico' />
+                    <input type='tel' className="w-[370px] h-[50px] border border-primary50 rounded-lg mx-[50px] px-4 py-3 mb-[25px] text-[16px] items-center" placeholder='Teléfono' />
+                    <input className="w-[370px] h-[130px] border border-primary50 rounded-lg text-start mx-[50px] p-5 mb-[25px] text-[16px] flex-start" placeholder='Escribe tu mensaje' />
                     <Button
                         type="filled"
                         onClick={handleClick}
-                        className="bg-accent w-[370px] h-[50px] text-white text-lg hover:bg-primary-dark"
+                        className="bg-accent w-[370px] h-[50px] mx-[50px] text-white text-[16px] mb-[50px] hover:bg-primary-dark"
                         disabled={false}
                     >
                         Enviar
                     </Button>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 export default Contacto;
