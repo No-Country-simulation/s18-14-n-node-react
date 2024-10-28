94bbf999e7352bcf8b8ba26731d254291a48b7ea
Milton Pulido <milton.pulido2018@gmail.com>
2024-10-27 23:29:12 -0500

    responsive sidebar

--- client/src/views/Navigation.tsx
+++ client/src/views/Navigation.tsx
@@ -1,21 +1,28 @@
 import Footer from "@/components/commons/Footer";
 import Navbar from "@/components/commons/Navbar";
 import { SideMenu } from "@/components/commons/SideMenu";
+import { useState } from "react";
 import { Outlet } from 'react-router-dom'
 
 export default function Navigation() {
+
+    const [open,setOpen] = useState(true);
+
     return (
         <div className="lg:h-screen h-full max-h-fit md:flex md:flex-col bg-[#EEE]">
             <Navbar />
 
             <div className="flex flex-row flex-grow overflow-hidden">
-                <div className="w-auto hidden md:block">
-                    <SideMenu />
+                <div className= {`${open? 'block':'-left-52' } z-10 lg:z-auto h-full lg:h-screen absolute lg:static`}>
+                    <SideMenu
+                    icon={open? "fa-solid fa-circle-chevron-right " : "fa-solid fa-circle-chevron-left "}
+                    action={()=>setOpen(!open)}
+                    />
                 </div>
                 <div className="flex-grow overflow-auto bg-colorblancoprincipal">
                     <Outlet 
                     />
-                    <div className="md:absolute relative md:top-full top-64  left-0 w-full bg-white z-10">
+                    <div className="lg:absolute relative lg:top-full top-64  left-0 w-full bg-white z-20">
                         <Footer />
                     </div>
                 </div>
